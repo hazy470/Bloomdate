ab414988924037f0b763e0ef2c215e743ee34249
hazy470 <hazelvaalz@gmail.com>
2025-04-29 10:21:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+});
+
+app.get('/swipe', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'swipe.html'));
+});
+
+app.get('/messages', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'messages.html'));
+});
+
+app.get('/settings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'settings.html'));
+});
+
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
-app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'views', 'signup.html')));
-app.get('/swipe', (req, res) => res.sendFile(path.join(__dirname, 'views', 'swipe.html')));
-app.get('/messages', (req, res) => res.sendFile(path.join(__dirname, 'views', 'messages.html')));
-app.get('/settings', (req, res) => res.sendFile(path.join(__dirname, 'views', 'settings.html')));
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
