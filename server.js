12adb66431ac7a0e1c474e24f9f07f058ab6f3e5
hazy470 <hazelvaalz@gmail.com>
2025-04-29 10:37:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Route for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
-});
-
+// Add more routes for your internal pages
 app.get('/swipe', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'swipe.html'));
 });
@@ -20,11 +19,15 @@ app.get('/messages', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'messages.html'));
 });
 
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'profile.html'));
+});
+
 app.get('/settings', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'settings.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
